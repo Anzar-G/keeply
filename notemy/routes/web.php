343e6d63300e7df834636209f2dd75b88fc49bef
Nizar <muhammadniyar282@gmail.com>
2026-01-10 13:00:01 +0700

    feat: make contact list the default homepage

--- notemy/routes/web.php
+++ notemy/routes/web.php
@@ -5,14 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [\App\Http\Controllers\ContactController::class, 'index'])->name('home');
 
 Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
